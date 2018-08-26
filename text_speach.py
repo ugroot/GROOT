2cf9c64ff3f224428c65527f3f066261cc9e28af
omkar yadav <omkar108591@gmail.com>
2018-08-26 23:54:58 +0530

    bug fix

--- text_speach.py
+++ text_speach.py
@@ -5,6 +5,6 @@ import os
 
 def speak_this(text_speak):
 	tts = gTTS(text_speak)
-	tts.save('speach.mp3')
-	playsound('speach.mp3')
-	os.remove('speach.mp3')
+	tts.save('speech.mp3')
+	playsound('speech.mp3')
+	os.remove('speech.mp3')
