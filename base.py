33d6327ca7a927d3562a8e7993976f0431dfc6f6
iamchiragsharma <chiragsharmagtbit@outlook.com>
2019-04-18 09:03:45 +0530

    "Face Recognition as a security system: Issue #41"

--- base.py
+++ base.py
@@ -1,10 +1,20 @@
 from PyQt5.QtWidgets import QApplication
+from faceUnlocker import unlock
+from texttospeech import speak_this
 import sys
+ 
 
 from Gui import Groot_Ui
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    groot = Groot_Ui()   #! Groot UI defined in the ui.py file
+    security = unlock()
+    if security == True:
+        speak_this("Master Found! Welcome Master!")
+        groot = Groot_Ui()   #! Groot UI defined in the Gui.py file
+    else:
+        speak_this("Imposter Found! Cannot Allow Access")
+        sys.exit(1)
+    
     sys.exit(app.exec_())
