475ae8793484bf8022613337eac36cad08278f36
iamchiragsharma <chiragsharmagtbit@outlook.com>
2019-05-01 23:38:08 +0530

    Branch Cleaned:  Removed Face Detection Overlap

--- base.py
+++ base.py
@@ -9,12 +9,5 @@ from Gui import Groot_Ui
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    security = unlock()
-    if security == True:
-        speak_this("Master Found! Welcome Master!")
-        groot = Groot_Ui()   #! Groot UI defined in the Gui.py file
-    else:
-        speak_this("Imposter Found! Cannot Allow Access")
-        sys.exit(1)
-    
+    groot = Groot_Ui()   #! Groot UI defined in the Gui.py file
     sys.exit(app.exec_())
